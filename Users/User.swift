37d02fb3ac415a964490bf5b818b474a77a0a7ae
kfurue <k.furue0728@gmail.com>
2017-09-02 23:55:34 +0900

    Add User class and first User unit test.

---
+++ Users/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  SampleApp
+//
+//  Created by Kazuhiro Furue on 2017/09/02.
+//  Copyright © 2017年 Kazuhiro Furue. All rights reserved.
+//
+
+import Foundation
+
+class User {
+
+}
