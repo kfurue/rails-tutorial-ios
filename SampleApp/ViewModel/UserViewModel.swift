3f8e59a824b01c2bbbc0d82e3a02170674a8127f
kfurue <k.furue0728@gmail.com>
2017-09-06 21:31:19 +0900

    Add User ViewModel

---
+++ SampleApp/ViewModel/UserViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserViewModel.swift
+//  SampleApp
+//
+//  Created by Kazuhiro Furue on 2017/09/06.
+//  Copyright © 2017年 Kazuhiro Furue. All rights reserved.
+//
+
+import Foundation
+
+final class UserViewModel {
+
+}
